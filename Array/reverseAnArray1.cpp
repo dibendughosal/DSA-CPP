c5b4b646230f7894715d03d8d6f2da4c0ee348a2
Dibendu Ghosal <128916258+dibendughosal@users.noreply.github.com>
2024-10-16 15:52:55 +0530

    DSA in C++

---
+++ Array/reverseAnArray1.cpp
@@ -0,0 +1,20 @@
+// Reverse an Array (Part 1)
+#include<iostream>
+using namespace std;
+int main(){
+    int a[5] = {11,12,13,14,17};
+    int b[5];
+    int j=0;
+    for (int i = 4; i >= 0; i--)
+    {
+        b[j] = a[i];
+        j++;
+    }
+    cout<<"the reverse element: ";
+    for (int j = 0; j < 5; j++)
+    {
+        cout<<b[j]<<" ";
+    }
+    
+    
+}
