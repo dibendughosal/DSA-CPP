344a7cdceca4201d886d1890f78efb3422cd9d1e
Dibendu Ghosal <128916258+dibendughosal@users.noreply.github.com>
2024-11-15 09:49:24 +0530

    DSA

--- pattern.cpp
+++ pattern.cpp
@@ -265,6 +265,30 @@ print14(int n){
     
 }
 print15(int n){
+    int iniSpace = (2*n) - 2;
+    for (int i = 1; i <= 2*n-1; i++)
+    {
+        int star = i;
+        if(i>n) star = 2*n - i;
+        for (int j = 1; j <= star; j++)
+        {
+            cout<<"*";
+        }
+        for (int j = 1; j <= iniSpace; j++)
+        {
+            cout<<" ";
+        }
+        for (int j = 1; j <= star; j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+        if(i<n) iniSpace -= 2;
+        else iniSpace += 2;
+        
+        
+        
+    }
     
 }
 int main(){
@@ -273,7 +297,7 @@ int main(){
     cin>>t;
     for(int i=0;i<t;i++){
         cin>>n;
-        print14(n);
+        print15(n);
     }
 
 }
